29d3d55a397b968c4003b87b82b6f2b5f7fb764a
Frikker <stolpnikk.97@gmail.com>
2019-08-08 11:47:09 +0300

    remove unnessecary code

--- app/models/team_match.rb
+++ app/models/team_match.rb
@@ -1,10 +1,7 @@
 class TeamMatch < ApplicationRecord
   has_many :player_statistics
 
-  belongs_to :team, foreign_key: 'opponent'
-
-  validates_associated :team
-  validates_presence_of :name, :date, :opponent
+  belongs_to :team
 
   before_validation do
     self.name = "#{team.name} vs. #{Team.find(opponent).name}.|
